1e8153e4597f5ca80211de1e9654f8be44d291ad
KyleWalkerley <walkerleykyle@gmail.com>
2021-02-17 08:44:45 +0200

    Chapters complete 17 Feb

---
+++ Chapter6/Try_it_yourself.py
@@ -0,0 +1,24 @@
+6.1
+#person_0 = {'first_Name': 'Jane','last_Name': 'Man','city_Living': 'Bronkhorstpruit', "age":41}
+
+#print(f"His name is {person_0['first_Name']}")
+#print(f"His surname is {person_0['last_Name']}")
+#print(f"He lives in {person_0['city_Living']}")
+#print(f"His current age is {person_0['age']}")
+
+6.2
+#favorite_numbers = {
+    #'Juan': ['18', '5'],
+    #'sarah': ['9'],
+    #'ewald': ['1'],
+    #'phillip': ['4', '25'],
+#}
+#for name, numbers in favorite_numbers.items():
+    #print(f"\n{name.title()}'s favorite numbers are:")
+    #for language in numbers:
+        #print(f"\t{language.title()}")
+
+
+6.3
+
+
